3229ec8804f673475c34b6fa96cd728228c95f8e
garrettcowley <garrettcowley@gmail.com>
2025-01-11 16:54:52 -0600

    Submission for W01 Programming Excercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter;
+
+        if (grade > 89)
+        {
+            letter = "A";
+        }
+        else if (grade > 79)
+        {
+            letter = "B";
+        }
+        else if (grade > 69)
+        {
+            letter = "C";
+        }
+        else if (grade > 59)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You have an {letter}.");
+        if (grade < 70)
+        {
+            Console.WriteLine("If you put in the effort, you can surely pass next time!");
+        }
+        else
+        {
+            Console.WriteLine("Great Job! Keep it up!");
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int input = -1;
+        List<int> responses = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (input != 0)
+        {
+            Console.Write("Enter Number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                responses.Add(input);
+            }
+        }
+
+        int total = 0;
+        foreach (int response in responses)
+        {
+            total += response;
+        }
+
+        int largest = 0;
+        foreach (int response in responses)
+        {
+            if (response > largest)
+            {
+                largest = response;
+            }
+        }
+
+        float average = (float)total / responses.Count;
+        Console.WriteLine($"The sum is {total}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The largest number is {largest}.");
     }
 }
